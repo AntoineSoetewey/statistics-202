2eacd3e4a7d58e982a22869125666d57a3d6a22e
Antoine Soetewey <ant.soetewey@gmail.com>
2020-03-09 11:55:32 +0100

    edit app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # statistics-202
-Shiny app for simple linear regression. More information: https://antoinesoetewey.shinyapps.io/statistics-202/
+Shiny app for simple linear regression. More information: https://antoinesoetewey.shinyapps.io/statistics-202/ and https://www.statsandr.com/blog/a-shiny-app-for-simple-linear-regression-by-hand-and-in-r/
--- app.R
+++ app.R
@@ -41,7 +41,7 @@ ui <- fluidPage(
       checkboxInput("echo", "Show code in report?", FALSE),
       downloadButton("downloadReport"),
       hr(),
-      HTML('<p>Report a <a href="https://github.com/AntoineSoetewey/statistics-202/issues">bug</a> or view the <a href="https://github.com/AntoineSoetewey/statistics-202/blob/master/app.R">code</a>. Back to <a href="https://www.antoinesoetewey.com/">www.antoinesoetewey.com</a>.</p>'),
+      HTML('<p>Report a <a href="https://github.com/AntoineSoetewey/statistics-202/issues">bug</a> or view the <a href="https://github.com/AntoineSoetewey/statistics-202/blob/master/app.R">code</a>. Back to <a href="https://www.antoinesoetewey.com/">www.antoinesoetewey.com</a> or <a href="https://www.statsandr.com/">www.statsandr.com</a>.</p>'),
       hr(),
       HTML('<a rel="license" href="http://creativecommons.org/licenses/by/2.0/be/" target="_blank"><img alt="Licence Creative Commons" style="border-width:0"
         src="http://i.creativecommons.org/l/by/2.0/be/80x15.png"/></a> Cette oeuvre de <span xmlns:cc="http://creativecommons.org/ns#"
