3aa33fe2b67b1c1940f471b1b9a8212c13ab9643
Antoine Soetewey <ant.soetewey@gmail.com>
2022-05-02 09:57:59 +0200

    improve UI

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 library(plotly)
@@ -15,63 +6,92 @@ library(knitr)
 library(pander)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-  # Application title
-  titlePanel("Statistics 202 - Simple linear regression"),
-  h4(tags$a(href = "https://antoinesoetewey.com/", "Antoine Soetewey")),
-  withMathJax(),
-
-  sidebarLayout(
-    sidebarPanel(
-      tags$b("Data:"),
-      textInput("x", "x", value = "90, 100, 90, 80, 87, 75", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
-      textInput("y", "y", value = "950, 1100, 850, 750, 950, 775", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
-      hr(),
-      tags$b("Plot:"),
-      checkboxInput("se", "Add confidence interval around the regression line", TRUE),
-      textInput("xlab", label = "Axis labels:", value = "x", placeholder = "x label"),
-      textInput("ylab", label = NULL, value = "y", placeholder = "y label"),
-      hr(),
-      # textInput("pred", "Prediction of y when x = ", value = "100, 110, 100, 90, 97, 85", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
-      # hr(),
-      radioButtons("format", "Download report:", c("HTML", "PDF", "Word"),
-        inline = TRUE
+ui <- shiny::tagList(
+  withMathJax(), 
+  includeCSS(path = "www/css/styles.css"), 
+  
+  tags$head(
+    tags$link(
+      rel = "shortcut icon", 
+      href = "https://antoinesoetewey.com/favicon.ico"
+    )
+  ), 
+  
+  tags$div(
+    tags$div(
+      class = "app_title", 
+      
+      titlePanel(
+        title = "Statistics 202 - Simple linear regression", 
+        windowTitle = "Simple linear regression"
       ),
-      checkboxInput("echo", "Show code in report?", FALSE),
-      downloadButton("downloadReport"),
-      hr(),
-      HTML('<p>Report a <a href="https://github.com/AntoineSoetewey/statistics-202/issues">bug</a> or view the <a href="https://github.com/AntoineSoetewey/statistics-202">code</a>. Back to <a href="https://antoinesoetewey.com/">antoinesoetewey.com</a> or <a href="https://statsandr.com/">statsandr.com</a>.</p>'),
-      hr(),
-      HTML('<a rel="license" href="http://creativecommons.org/licenses/by/2.0/be/" target="_blank"><img alt="Licence Creative Commons" style="border-width:0"
-        src="http://i.creativecommons.org/l/by/2.0/be/80x15.png"/></a> This work of <span xmlns:cc="http://creativecommons.org/ns#"
-        property="cc:attributionName"><font face="Courier">RShiny@UCLouvain</font></span> is made available under the terms of the <a rel="license"
-        href="http://creativecommons.org/licenses/by/2.0/be/" target="_blank">Creative Commons Attribution 2.0 Belgium license</a>. Details on the use of this resource on <a href="http://sites.uclouvain.be/RShiny"
-        target="_blank"><font face="Courier">RShiny@UCLouvain</font></a>. Source code available on <a href="https://github.com/AntoineSoetewey/statistics-202" target="_blank">GitHub</a>.')
+      
+      tags$h4(
+        tags$a(
+          href = "https://antoinesoetewey.com/", 
+          target = "_blank", 
+          "Antoine Soetewey"
+        )
+      )
     ),
-
-    mainPanel(
-      tags$b("Your data:"),
-      DT::dataTableOutput("tbl"),
-      br(),
-      uiOutput("data"),
-      br(),
-      tags$b("Compute parameters by hand:"),
-      uiOutput("by_hand"),
-      br(),
-      tags$b("Compute parameters in R:"),
-      verbatimTextOutput("summary"),
-      br(),
-      tags$b("Regression plot:"),
-      uiOutput("results"),
-      plotlyOutput("plot"),
-      br(),
-      tags$b("Interpretation:"),
-      uiOutput("interpretation"),
-      br(),
-      br()
+    
+    # Sidebar with a slider input for number of bins
+    fluidPage(
+      theme = shinythemes::shinytheme("flatly"),
+      sidebarLayout(
+        sidebarPanel(
+          tags$b("Data:"),
+          textInput("x", "x", value = "90, 100, 90, 80, 87, 75", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
+          textInput("y", "y", value = "950, 1100, 850, 750, 950, 775", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
+          hr(),
+          tags$b("Plot:"),
+          checkboxInput("se", "Add confidence interval around the regression line", TRUE),
+          textInput("xlab", label = "Axis labels:", value = "x", placeholder = "x label"),
+          textInput("ylab", label = NULL, value = "y", placeholder = "y label"),
+          hr(),
+          # textInput("pred", "Prediction of y when x = ", value = "100, 110, 100, 90, 97, 85", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
+          # hr(),
+          radioButtons("format", "Download report:", c("HTML", "PDF", "Word"),
+                       inline = TRUE
+          ),
+          checkboxInput("echo", "Show code in report?", FALSE),
+          downloadButton("downloadReport")
+        ),
+        
+        mainPanel(
+          br(),
+          tags$b("Data:"),
+          DT::dataTableOutput("tbl"),
+          br(),
+          uiOutput("data"),
+          br(),
+          tags$b("Compute parameters by hand:"),
+          uiOutput("by_hand"),
+          br(),
+          tags$b("Compute parameters in R:"),
+          verbatimTextOutput("summary"),
+          br(),
+          tags$b("Regression plot:"),
+          uiOutput("results"),
+          plotlyOutput("plot"),
+          br(),
+          tags$b("Interpretation:"),
+          uiOutput("interpretation"),
+          br(),
+          br()
+        )
+      )
+    )
+  ), 
+  
+  tags$footer(
+    tags$div(
+      class = "footer_container", 
+      
+      includeHTML(path = "www/html/footer.html")
     )
   )
+  
 )
 
 server <- function(input, output) {
